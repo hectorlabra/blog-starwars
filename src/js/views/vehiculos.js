1e19e0ab12593fdec0b2c89bbf66e05fb375067f
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-09-14 16:13:16 -0300

    creating vehicle information view

--- src/js/views/vehiculos.js
+++ src/js/views/vehiculos.js
@@ -9,7 +9,7 @@ const Vehiculos = () => {
 
   const { theid } = useParams();
 
-  const [vehiculos, setVehiculos] = useState([]);
+  const [vehiculos, setVehiculos] = useState(null);
 
   const isFavorite =
     vehiculos && favorites.some((fav) => fav.id === vehiculos.id);
@@ -31,22 +31,22 @@ const Vehiculos = () => {
         return response.json();
       })
       .then((data) => {
-        // Guarda los detalles del personaje en el estado
+        // Guarda los detalles del vehiculo en el estado
         setVehiculos(data);
       })
       .catch((error) => {
-        console.error("Error fetching character details:", error);
+        console.error("Error fetching vehicle details:", error);
       });
   }, [theid]);
 
-  const getVehiculosImage = () => {
+  const getVehiculoImage = () => {
     if (!vehiculos) return null;
-    const vehiculosId = vehiculos?.url?.split("/").filter(Boolean).slice(-1)[0];
-    return `https://starwars-visualguide.com/assets/img/vehicles/${vehiculosId}.jpg`;
+    const vehiculoId = vehiculos?.url?.split("/").filter(Boolean).slice(-1)[0];
+    return `https://starwars-visualguide.com/assets/img/vehicles/${vehiculoId}.jpg`;
   };
 
   return (
-    <div>
+    <div className="bg-dark text-light min-vh-100">
       <div className="container-fluid">
         <Navbar
           favorites={favorites}
@@ -55,62 +55,61 @@ const Vehiculos = () => {
         />
       </div>
       {vehiculos ? (
-        <>
-          <div className="container">
-            <div className="row ">
-              <div className="col-12 col-md-4">
-                <img
-                  src={getVehiculosImage()}
-                  className="img-fluid rounded-start w-100"
-                  alt={vehiculos.name}
-                />
-              </div>
-              <div className="col-12 col-md-8 ">
-                <div className="card-body-single text-center m-5">
-                  <h1 className="card-title">{vehiculos.name}</h1>
-                  <p className="card-text">
-                    <b>Modelo:</b> {vehiculos.model}
-                  </p>
-                  <p className="card-text">
-                    <b>Fabricante:</b> {vehiculos.manufacturer}
-                  </p>
-                  <p className="card-text">
-                    <b>Costo en Credito:</b> {vehiculos.cost_in_credits}
-                  </p>
-                  <p className="card-text">
-                    <b>Pasajeros:</b> {vehiculos.passengers}
-                  </p>
-                  <p className="card-text">
-                    <b>Clase de vehiculo:</b> {vehiculos.vehicle_class}
-                  </p>
-                  <p className="card-text">
-                    <b>Capacidad de carga:</b> {vehiculos.cargo_capacity}
-                  </p>
-                  <p className="card-text">
-                    <b>Longitud:</b> {vehiculos.length}
-                  </p>
-                  <p className="card-text">
-                    <b>Consumibles:</b> {vehiculos.consumables}
-                  </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <img
+                src={getVehiculoImage()}
+                className="img-fluid rounded-start w-100"
+                alt={vehiculos.name}
+              />
+            </div>
+            <div className="col-12 col-md-8">
+              <div className="card-body-single text-center m-5">
+                <h1 className="card-title">{vehiculos.name}</h1>
+                <p className="card-text">
+                  <b>Modelo:</b> {vehiculos.model}
+                </p>
+                <p className="card-text">
+                  <b>Fabricante:</b> {vehiculos.manufacturer}
+                </p>
+                <p className="card-text">
+                  <b>Costo en Crédito:</b> {vehiculos.cost_in_credits}
+                </p>
+                <p className="card-text">
+                  <b>Pasajeros:</b> {vehiculos.passengers}
+                </p>
+                <p className="card-text">
+                  <b>Clase de vehículo:</b> {vehiculos.vehicle_class}
+                </p>
+                <p className="card-text">
+                  <b>Capacidad de carga:</b> {vehiculos.cargo_capacity}
+                </p>
+                <p className="card-text">
+                  <b>Longitud:</b> {vehiculos.length}
+                </p>
+                <p className="card-text">
+                  <b>Consumibles:</b> {vehiculos.consumables}
+                </p>
 
-                  <button className="btnsingle" onClick={handleToggleFavorite}>
-                    {isFavorite
-                      ? "Eliminar de favoritos"
-                      : "Agregar a favorito"}
-                  </button>
-                  <br />
-                  <br />
-                  <button className="btnsingle">
-                    <Link to="/">Volver</Link>
-                  </button>
-                </div>
+                <button
+                  className={`btn btn-${isFavorite ? "danger" : "success"}`}
+                  onClick={handleToggleFavorite}
+                >
+                  {isFavorite ? "Eliminar de favoritos" : "Agregar a favorito"}
+                </button>
+                <br />
+                <br />
+                <button className="btn btn-light">
+                  <Link to="/">Volver</Link>
+                </button>
               </div>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <h2 className="text-white text-center">
-          {vehiculos === null ? "Cargando..." : "Character not found"}
+          {vehiculos === null ? "Cargando..." : "Vehículo no encontrado"}
         </h2>
       )}
     </div>
