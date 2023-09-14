f31f624077d11a264405b6d57c80a9962b999a97
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-09-14 16:10:36 -0300

    creating planets information view

--- src/js/views/planeta.js
+++ src/js/views/planeta.js
@@ -7,7 +7,7 @@ const Planeta = () => {
   const { favorites, addToFavorites, removeFromFavorites } =
     useContext(AppContext);
 
-  const [planeta, setplaneta] = useState(null);
+  const [planeta, setPlaneta] = useState(null);
 
   const { theid } = useParams();
 
@@ -30,22 +30,21 @@ const Planeta = () => {
         return response.json();
       })
       .then((data) => {
-        // Guarda los detalles del personaje en el estado
-        setplaneta(data);
+        setPlaneta(data);
       })
       .catch((error) => {
         console.error("Error fetching planeta details:", error);
       });
   }, [theid]);
 
-  const getplanetaImage = () => {
+  const getPlanetaImage = () => {
     if (!planeta) return null;
     const planetaId = planeta.url.split("/").filter(Boolean).slice(-1)[0];
     return `https://starwars-visualguide.com/assets/img/planets/${planetaId}.jpg`;
   };
 
   return (
-    <div>
+    <div className="bg-dark text-light min-vh-100">
       <div className="container-fluid">
         <Navbar
           favorites={favorites}
@@ -54,59 +53,58 @@ const Planeta = () => {
         />
       </div>
       {planeta ? (
-        <>
-          <div className="container">
-            <div className="row ">
-              <div className="col-12 col-md-4">
-                <img
-                  src={getplanetaImage()}
-                  className="img-fluid rounded-start w-100"
-                  alt={planeta.name}
-                />
-              </div>
-              <div className="col-12 col-md-8 ">
-                <div className="card-body-single text-center m-5">
-                  <h1 className="card-title">{planeta.name}</h1>
-                  <p className="card-text">
-                    <b>Periodo de rotacion:</b> {planeta.rotation_period}
-                  </p>
-                  <p className="card-text">
-                    <b>Diametro:</b> {planeta.diameter}
-                  </p>
-                  <p className="card-text">
-                    <b>Clima:</b> {planeta.climate}
-                  </p>
-                  <p className="card-text">
-                    <b>Gravedad:</b> {planeta.gravity}
-                  </p>
-                  <p className="card-text">
-                    <b>Terreno:</b> {planeta.terrain}
-                  </p>
-                  <p className="card-text">
-                    <b>Superficie del agua:</b> {planeta.surface_water}
-                  </p>
-                  <p className="card-text">
-                    <b>Población:</b> {planeta.population}
-                  </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <img
+                src={getPlanetaImage()}
+                className="img-fluid rounded-start w-100"
+                alt={planeta.name}
+              />
+            </div>
+            <div className="col-12 col-md-8">
+              <div className="card-body-single text-center m-5">
+                <h1 className="card-title">{planeta.name}</h1>
+                <p className="card-text">
+                  <b>Periodo de rotación:</b> {planeta.rotation_period}
+                </p>
+                <p className="card-text">
+                  <b>Diámetro:</b> {planeta.diameter}
+                </p>
+                <p className="card-text">
+                  <b>Clima:</b> {planeta.climate}
+                </p>
+                <p className="card-text">
+                  <b>Gravedad:</b> {planeta.gravity}
+                </p>
+                <p className="card-text">
+                  <b>Terreno:</b> {planeta.terrain}
+                </p>
+                <p className="card-text">
+                  <b>Superficie del agua:</b> {planeta.surface_water}
+                </p>
+                <p className="card-text">
+                  <b>Población:</b> {planeta.population}
+                </p>
 
-                  <button className="btnsingle" onClick={handleToggleFavorite}>
-                    {isFavorite
-                      ? "Eliminar de favoritos"
-                      : "Agregar a favorito"}
-                  </button>
-                  <br />
-                  <br />
-                  <button className="btnsingle">
-                    <Link to="/">Volver</Link>
-                  </button>
-                </div>
+                <button
+                  className={`btn btn-${isFavorite ? "danger" : "success"}`}
+                  onClick={handleToggleFavorite}
+                >
+                  {isFavorite ? "Eliminar de favoritos" : "Agregar a favorito"}
+                </button>
+                <br />
+                <br />
+                <button className="btn btn-light">
+                  <Link to="/">Volver</Link>
+                </button>
               </div>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <h2 className="text-white text-center">
-          {planeta === null ? "Cargando..." : "Character not found"}
+          {planeta === null ? "Cargando..." : "Planeta no encontrado"}
         </h2>
       )}
     </div>
