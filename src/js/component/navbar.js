dba6fac6b6ea5b66be09f58548c49f1b1e9e4e57
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-09-14 16:08:23 -0300

    creating character information view

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,13 +2,11 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import star from "../component/img/otra.png";
 import { AppContext } from "../store/appContext";
-
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
-  const { favorites, addToFavorites, removeFromFavorites } =
-    useContext(AppContext);
+  const { favorites, removeFromFavorites } = useContext(AppContext);
 
   return (
     <nav
@@ -17,40 +15,43 @@ const Navbar = () => {
     >
       <div className="container-fluid">
         <Link to="/">
-          <img className="logo" src={star} alt="Star"></img>
+          <img className="logo" src={star} alt="Star" />
         </Link>
 
-        <div className="">
-          <div className=" dropdown">
+        <div className="d-flex align-items-center">
+          <div className="dropdown">
             <button
-              className=" btndrop dropdown-toggle"
+              className="btn btn-secondary dropdown-toggle"
               type="button"
               id="favoritesDropdown"
               data-bs-toggle="dropdown"
               aria-expanded="false"
+              style={{ backgroundColor: "#000000", borderColor: "#ffffff" }}
             >
               Favoritos ({favorites.length})
             </button>
-            <ul className="dropdown-menu" aria-labelledby="favoritesDropdown">
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="favoritesDropdown"
+            >
               {favorites.length > 0 ? (
                 favorites.map((item) => (
                   <li key={item.url}>
-                    <p className="favorite-item">
-                      {item.name}
+                    <div className="d-flex justify-content-between align-items-center">
+                      <p className="favorite-item m-0">{item.name}</p>
                       <button
-                        className="btn-delete"
+                        className="btn btn-danger btn-sm"
                         onClick={() => removeFromFavorites(item)}
                       >
                         <FontAwesomeIcon icon={faTrash} />
                       </button>
-                    </p>
+                    </div>
                   </li>
                 ))
               ) : (
                 <li>
-                  <p className="dropdown-item text-center">
-                    No hay personajes <br />
-                    favoritos
+                  <p className="dropdown-item text-center m-0">
+                    No hay personajes <br /> favoritos
                   </p>
                 </li>
               )}
--- src/js/views/personajes.js
+++ src/js/views/personajes.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { AppContext } from "../store/appContext";
-
 import { Navbar } from "../component/navbar";
 
 const Personajes = () => {
@@ -32,7 +31,6 @@ const Personajes = () => {
         return response.json();
       })
       .then((data) => {
-        // Guarda los detalles del personaje en el estado
         setCharacter(data);
       })
       .catch((error) => {
@@ -47,7 +45,7 @@ const Personajes = () => {
   };
 
   return (
-    <div>
+    <div className="bg-dark text-light min-vh-100">
       <div className="container-fluid">
         <Navbar
           favorites={favorites}
@@ -56,56 +54,55 @@ const Personajes = () => {
         />
       </div>
       {character ? (
-        <>
-          <div className="container">
-            <div className="row ">
-              <div className="col-12 col-md-4">
-                <img
-                  src={getCharacterImage()}
-                  className="img-fluid rounded-start w-100"
-                  alt={character.name}
-                />
-              </div>
-              <div className="col-12 col-md-8 ">
-                <div className="card-body-single text-center m-5">
-                  <h1 className="card-title">{character.name}</h1>
-                  <p className="card-text">
-                    <b>Altura:</b> {character.height} cm
-                  </p>
-                  <p className="card-text">
-                    <b>Peso:</b> {character.mass} kg
-                  </p>
-                  <p className="card-text">
-                    <b>Color de pelo:</b> {character.hair_color}
-                  </p>
-                  <p className="card-text">
-                    <b>Color de piel:</b> {character.skin_color}
-                  </p>
-                  <p className="card-text">
-                    <b>Color ojos:</b> {character.eye_color}
-                  </p>
-                  <p className="card-text">
-                    <b>Año de nacimiento:</b> {character.birth_year}
-                  </p>
-                  <p className="card-text">
-                    <b>Género:</b> {character.gender}
-                  </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <img
+                src={getCharacterImage()}
+                className="img-fluid rounded-start w-100"
+                alt={character.name}
+              />
+            </div>
+            <div className="col-12 col-md-8">
+              <div className="card-body-single text-center m-5">
+                <h1 className="card-title">{character.name}</h1>
+                <p className="card-text">
+                  <b>Altura:</b> {character.height} cm
+                </p>
+                <p className="card-text">
+                  <b>Peso:</b> {character.mass} kg
+                </p>
+                <p className="card-text">
+                  <b>Color de pelo:</b> {character.hair_color}
+                </p>
+                <p className="card-text">
+                  <b>Color de piel:</b> {character.skin_color}
+                </p>
+                <p className="card-text">
+                  <b>Color ojos:</b> {character.eye_color}
+                </p>
+                <p className="card-text">
+                  <b>Año de nacimiento:</b> {character.birth_year}
+                </p>
+                <p className="card-text">
+                  <b>Género:</b> {character.gender}
+                </p>
 
-                  <button className="btnsingle" onClick={handleToggleFavorite}>
-                    {isFavorite
-                      ? "Eliminar de favoritos"
-                      : "Agregar a favorito"}
-                  </button>
-                  <br />
-                  <br />
-                  <button className="btnsingle">
-                    <Link to="/">Volver</Link>
-                  </button>
-                </div>
+                <button
+                  className={`btn btn-${isFavorite ? "danger" : "success"}`}
+                  onClick={handleToggleFavorite}
+                >
+                  {isFavorite ? "Eliminar de favoritos" : "Agregar a favorito"}
+                </button>
+                <br />
+                <br />
+                <button className="btn btn-light">
+                  <Link to="/">Volver</Link>
+                </button>
               </div>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <h2 className="text-white text-center">
           {character === null ? "Cargando..." : "Character not found"}
